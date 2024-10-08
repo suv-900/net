ea967852c955098a10e1471b7ed84cdc651bc2ea
core <suvhampaul36@gmail.com>
2024-10-08 12:24:52 +0530

    needed abstraction

---
+++ internal/models.go
@@ -0,0 +1,44 @@
+package internal
+
+import(
+"context"
+"os"
+"log"
+)
+type Models struct{
+	User interface{
+		
+	}
+}
+
+const(
+	ErrInternalServerError = errors.New("internal server error.")
+)
+
+func CreateModel()(*Models,error){
+	
+	dburl,exists := os.LookupEnv("dburl")
+	if !exists{
+		log.Print("env var dburl doesnt exists")
+		return nil,ErrInternalServerError
+	}
+	
+	conn,err := CreatePGConn(context.Background(),dburl)
+	if err != nil{
+		return nil,err
+	}
+	
+	models := Models{
+		User: UserRepo{conn:conn}
+	}
+
+	return *models,nil
+}
+
+
+
+
+
+
+
+
